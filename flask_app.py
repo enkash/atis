797045eff544e0ec62bff494170873137c2dfc5f
enkash <nikhil84@gmail.com>
2018-09-19 11:07:45 +0530

    adding main file flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,12 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
+import atis_india
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+@app.route('/<icao>')
+def landing(icao):
+	return atis_india.get_atis(icao)
 
+@app.route('/getatis')
+def get_atis():
+	return 'atis of'
